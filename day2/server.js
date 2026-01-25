adc86ed1b30068ccbd262ce499a625691992aca3
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-01-25 16:39:14 +0530

    done

---
+++ day2/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+
+const app = express() 
+
+app.get('/',(req,res)=>{
+    res.send('hello serverrrr')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('hello serverrrr')
+})
+app.listen(3000 )
+
+
