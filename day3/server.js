3211535d6e9459b88b4da94183a0426859256884
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-01-25 18:24:51 +0530

    mini api

---
+++ day3/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  res.send("notes created");
+});
+
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
