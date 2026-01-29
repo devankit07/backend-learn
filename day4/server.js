d34c72569334f5bdf69af7dbfb13907fe8cd0c01
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-01-29 11:46:39 +0530

    ....

--- day4/server.js
+++ day4/server.js
@@ -2,15 +2,7 @@
 
 const app = require("./src/app")
 
-const notes = []
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
-})
-
-app.post("/notes",(req,res)=>{
-    res.send("notes sended")
-})
 
 app.listen(3000,()=>{
     console.log("server is runing on port 3000 ")
--- day4/src/app.js
+++ day4/src/app.js
@@ -1,8 +1,38 @@
-// server ko create and config app.js mai krna 
+// server ko create and config app.js mai krna
 
-const express  = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
+//middleware
+app.use(express.json())
+
+const notes = [];
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  console.log(notes)
+
+  res.send("notes sended");
+});
+
+app.get("/notes",(req,res)=>{
+    res.send(notes)
+})
 
 
-module.exports = app
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index]
+    res.send("notes deleted")
+})
+
+
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].content = req.body.content
+    res.send("updated")
+})
+module.exports = app;
